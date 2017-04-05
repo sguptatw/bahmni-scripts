dfa5ac5e93cfebb1bda7e0bee2641a015e379e1a
Shashikanth Gadgay <shashikanthgadgay@gmail.com>
2017-04-05 16:10:30 +0530

    Removing all emrapi artifacts

--- ci/pre_build_tasks.sh
+++ ci/pre_build_tasks.sh
@@ -4,7 +4,7 @@ rm -rf /var/go/.m2/repository/org/openmrs/module/bahmnimapping
 rm -rf /var/go/.m2/repository/org/openmrs/module/bedmanagement*
 rm -rf /var/go/.m2/repository/org/openmrs/module/bahmnicore
 rm -rf /var/go/.m2/repository/org/openmrs/module/bahmnicoreui
-rm -rf /var/go/.m2/repository/org/openmrs/module/emrapi
+rm -rf /var/go/.m2/repository/org/openmrs/module/emrapi*
 rm -rf /var/go/.m2/repository/org/openmrs/module/bacteriology
 rm -rf /var/go/.m2/repository/org/openmrs/module/bacteriology-api
 rm -rf /var/go/.m2/repository/org/openmrs/module/bacteriology-omod
